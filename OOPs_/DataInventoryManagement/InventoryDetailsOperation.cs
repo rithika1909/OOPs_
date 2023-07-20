6a3b643119fefe17c66e023c198380851ead81ee
Rithika L <rithika.l@tvsnext.com>
2023-07-20 13:22:48 +0530

    [Rithika] Add : Implemented

---
+++ OOPs_/DataInventoryManagement/InventoryDetailsOperation.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPs_.DataInventoryManagement
+{
+    public class InventoryDetailsOperation
+    {
+        public void ReadInventoryJson(string filePath)
+        {
+            var json = File.ReadAllText(filePath);
+            List<InventoryDetails> list = JsonConvert.DeserializeObject<List<InventoryDetails>>(json);
+            foreach (var data in list)
+            {
+                Console.WriteLine(data.Name + " " + data.Weight + " " + data.PricePerKg);
+            }
+        }
+    }
+}
--- OOPs_/Program.cs
+++ OOPs_/Program.cs
@@ -1,11 +1,31 @@
-﻿using System;
+﻿using OOPs_.DataInventoryManagement;
+using System;
 namespace OOPs_
 {
     class Program
     {
+        static string DataInventoryManagement_filePath = @"D:\OOPs_\OOPs_\DataInventoryManagement\InventoryData.json";
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to OOPs");
+
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter the option:\n1.Data Inventory Management\n 2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        InventoryDetailsOperation detailsOperation = new InventoryDetailsOperation();
+                        detailsOperation.ReadInventoryJson(DataInventoryManagement_filePath);
+                        break;
+                    default:
+                        flag = false;
+                        break;
+
+
+                }
+            }
         }
     }
 }
