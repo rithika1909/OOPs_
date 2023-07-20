09af999e387f237e1e4da4e17e2b431dd51ffb4c
Rithika L <rithika.l@tvsnext.com>
2023-07-20 13:04:04 +0530

    [Rithika] Add : Implemented

---
+++ OOPs_/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace OOPs_
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to OOPs");
+        }
+    }
+}
